215ded6a88817ca7c28429f3e6da895106d54259
Akrishnaveni <annakrishnaveni05@gmail.com>
2023-09-16 10:02:35 +0000

    reducedFiles

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Home from './components/Home'
+import './App.css'
+
+// This is the list (static data) used in the application. You can move it to any component if needed.
+
+// Replace your code here
+const App = () => <Home />
+
+export default App
